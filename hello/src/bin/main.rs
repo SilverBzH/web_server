2d02bf3529e0a64ca0422b9ab65810f0da3cb4a1
Charlie <charlie.boutier7@gmail.com>
2020-04-19 19:56:23 +0200

    Implement worker struct

--- hello/src/bin/main.rs
+++ hello/src/bin/main.rs
@@ -3,12 +3,18 @@ use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
 
+use hello::Threadpool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let threadpool = Threadpool::new(4);
+    
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         // println!("Connection established!");
-        handle_connection(stream);
+        threadpool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
---
+++ hello/src/lib.rs
@@ -0,0 +1,40 @@
+use std::thread;
+
+pub struct Threadpool {
+    workers: Vec<Worker>,
+}
+
+impl Threadpool {
+    /// Create a new ThreadPool.
+    /// 
+    /// The size is the number of threads in the pool.
+    /// 
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> Threadpool {
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+        Threadpool {workers}
+    }
+
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static
+    {
+        
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker{id, thread}
+    }
+}
